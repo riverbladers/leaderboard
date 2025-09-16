90b6cbd2252b525a2d4a1634a6eab1ac6c67a535
Butch Paolo Madahan <butchpaolom@gmail.com>
2025-09-16 20:04:11 +0800

    v1

--- scripts.js
+++ scripts.js
@@ -1,65 +1,77 @@
-fetch('data/stats.json')
-    .then(response => response.json())
-    .then(data => {
-        const tags = {
-            'hydra': 'bg-primary',
-            'rivers': 'bg-info',
-        };
-        const tbody = document.getElementById('leaderboard-body');
-        tbody.innerHTML = '';
-        const sortedPlayers = data.groupStage.slice().sort((a, b) => {
-            const diffA = a.wins - a.loses;
-            const diffB = b.wins - b.loses;
-            return diffB - diffA;
-        });
-
-        setTimeout(() => {
-            sortedPlayers.forEach((player, idx) => {
-                setTimeout(() => {
-                    const diff = player.wins - player.loses;
-                    const diffClass = diff > 0 ? 'text-success' : diff < 0 ? 'text-danger' : '';
-                    const tr = document.createElement('tr');
-                    if (idx % 2 === 0) tr.classList.add('table-active');
-                    tr.classList.add('animate__animated', 'animate__backInLeft');
-                    let tagKey = Object.keys(tags).find(tag => player.name.toLowerCase().includes(tag));
-                    let badge = '-';
-                    if (tagKey) {
-                        badge = `<span class="badge rounded-pill ${idx === 0 ? 'bg-dark' : `${tags[tagKey]}`}">${tagKey}</span>`
-                    }
-                    if (idx === 0) {
-                        tr.classList.add('table-primary');
-                    }
-                    tr.innerHTML = `
-                    <th scope="row">${idx === 0 ? '👑' : idx + 1}</th>
-                    <td>${badge}</td>
-                    <td><h6 ${idx === 0 ? 'class="animate__animated animate__pulse animate__infinite"' : ''}>${player.name}</h6></td>
-                    <td class="text-success text-center">${player.wins}</td>
+const loadData = (data) => {
+        document.getElementById('leaderboard-header').style.display = 'block';
+    const tags = {
+        'HYDRA': 'bg-primary',
+        'RIVERS': 'bg-info',
+        'T2': 'bg-success',
+    };
+    const leaderboardBody = document.getElementById('leaderboard-body');
+    leaderboardBody.innerHTML = '';
+    data.forEach((player, idx) => {
+        if (idx > 19) return;
+        const div = document.createElement('div');
+        div.classList.add('animate__animated', 'row', 'd-flex', 'justify-content-between');
+        let tagKey = Object.keys(tags).find(tag => player.name.toUpperCase().includes(tag));
+        let badge = '-';
+        if ([1, 2, 3].includes(player.rank)) {
+            div.classList.add(`animate__delay-${(4-player.rank)*250}ms`, 'animate__lightSpeedInLeft', 'animate__faster');
+        } else {
+            div.classList.add('animate__fadeInDown');
+        }
+        if (tagKey) {
+            badge = `<span class="badge rounded-pill ${tags[tagKey]}">${tagKey}</span>`
+        }
+        div.innerHTML = `
+                        <div class="rank col-2 text-center">${player.rank === 1 ? '👑' : player.rank}</div>
+                        <div class="blader-name col-6 text-truncate ${player.rank === 1 ? 'animate__flash animate__animated animate__infinite animate__slower' : ''}">${player.name}</div>
+                        <div class="group col-2 text-center">${badge}</div>
+                        <div class="wins col-2 text-center">${player.wins}</div>
                     `;
-                    tbody.appendChild(tr);
-                }, idx * 100);
-            });
-        }, 2000);
-    })
-    .catch(error => {
-        console.error('Error loading JSON:', error);
+        leaderboardBody.appendChild(div);
     });
+}
+
+const data = [];
+
+async function fetchData() {
+    const response = await fetch("data/stats.json");
+    const a = await response.json();
+    data.push(...a['groupStage']);
+    loadData(data);
+}
+
+setTimeout(() => {
+    fetchData();
+}, 1200);
+
 
 window.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
+    setTimeout(() => {
         var overlay = document.getElementById('loading-overlay');
         if (overlay) overlay.style.display = 'none';
-    }, 2000);
+    }, 1200);
+    let valueNow = 0;
+    let loader = setInterval(() => {
+        var loadingBar = document.getElementById('loading-bar');
+        loadingBar.style.width = valueNow + '%';
+        valueNow += 1;
+    }, 2);
+    setTimeout(() => { clearInterval(loader); }, 800);
 });
 
+const emptyState = () => {
+    document.getElementById('leaderboard-header').style.display = 'none';
+    const leaderboardBody = document.getElementById('leaderboard-body');
+    leaderboardBody.innerHTML = '';
+    const div = document.createElement('div');
+    div.innerHTML = `<div class="col-12 text-center mt-4 animate__animated animate__flash animate__infinite">No results found</div>`;
+    leaderboardBody.appendChild(div);
+}
 document.getElementById('search-input').addEventListener('input', function () {
-    const filter = this.value.toLowerCase();
-    const rows = document.querySelectorAll('#leaderboard-body tr');
-    rows.forEach(row => {
-        const nameCell = row.querySelector('td:nth-child(3)');
-        if (nameCell && nameCell.textContent.toLowerCase().includes(filter)) {
-            row.style.display = '';
-        } else {
-            row.style.display = 'none';
-        }
-    });
+    const leaderboardBody = document.getElementById('leaderboard-body');
+    leaderboardBody.innerHTML = '';
+    filteredData = data.filter(player => player.name.toLowerCase().includes(this.value.toLowerCase()));
+    filteredData.length !== 0 ? loadData(filteredData) : emptyState();
 });
+
+
